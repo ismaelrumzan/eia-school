b3672d9e946228f8efff0129a1b46257b6d67f5a
Ismael Rumzan <ismael.rumzan@gmail.com>
2023-12-15 01:07:41 -0700

    first version

---
+++ components/ui/submit-form.tsx
@@ -0,0 +1,10 @@
+export function SubmitForm() {
+  return (
+    <iframe
+      src="https://docs.google.com/forms/d/e/1FAIpQLSfnBGf3yezoNCQf0hcrWeSsMr7aN12gkpeUB6VeNXiLEPA3wA/viewform?embedded=true"
+      width="100%"
+      className="min-h-[600px]">
+      Loading…
+    </iframe>
+  );
+}
