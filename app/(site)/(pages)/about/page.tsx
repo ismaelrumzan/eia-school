acfd760f3885e02d9386803a121a3ee90845ca99
ismael.rumzan <ismael.rumzan@gmail.com>
2024-04-04 06:48:06 -0600

    about

--- app/(site)/(pages)/about/page.tsx
+++ app/(site)/(pages)/about/page.tsx
@@ -1,18 +1,13 @@
 import { CarouselVideo } from "@/components/ui/carousel-video";
-import { ArrowLeft } from "lucide-react";
-import { NavButton } from "@/components/ui/buttons/nav-button";
+import { H1 } from "@/components/ui/typography/h1";
 
 export default function Page() {
     return (
-        <>
-            <div className="pl-3 mb-3">
-                <NavButton href="/">
-                    <ArrowLeft />
-                </NavButton>
-            </div>
+        <div className="pb-4">
+        <H1 className="my-3">About Us</H1>
             <div className="p-2">
                 <CarouselVideo id="dEDcNA-vHvo" text="About the Edmonton Islamic Academy - Elementary Campus" params="start=90" />
             </div>
-        </>
+        </div>
     );
 }
