258d5f9e47a5dce9da4cb84185a95f63758686bf
ismaelrumzan <ismael.rumzan@gmail.com>
2024-04-06 05:41:26 -0600

    track donate button

--- components/ui/buttons/nav-button.tsx
+++ components/ui/buttons/nav-button.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { sendGTMEvent } from "@next/third-parties/google";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
@@ -12,7 +14,13 @@ export function NavButton({
     <Button
       asChild
       className="font-bebas text-[#094931] text-[20px] uppercase bg-[#C6BB79] hover:bg-[#DED6A5]">
-      <Link href={href}>{children}</Link>
+      <Link
+        href={href}
+        onClick={() =>
+          sendGTMEvent({ event: "buttonClicked", value: "topnav-donate" })
+        }>
+        {children}
+      </Link>
     </Button>
   );
 }
